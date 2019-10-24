ec9128782427a7bbf8fc97be30484c428a14e97f
owenmadin <owen.madin@colorado.edu>
2019-10-24 11:38:51 -0600

    Added tests for stdev method

--- basics_test.py
+++ basics_test.py
@@ -22,7 +22,27 @@ class TestMean(unittest.TestCase):
             rand = []
             for j in range(length):
                 rand.append(random.randint(1,1000))
-            self.assertEqual(get_column_stats.calculate_mean(rand),np.mean(rand))
-            
+            self.assertAlmostEqual(get_column_stats.calculate_mean(rand),np.mean(rand))
+
+class TestStdev(unittest.TestCase):
+    def test_no_input(self):
+        self.assertRaises(TypeError,get_column_stats.calculate_stdev,None)
+    def test_wrong_types(self):
+        self.assertRaises(TypeError,get_column_stats.calculate_stdev,1)
+        self.assertRaises(TypeError,get_column_stats.calculate_stdev,'text')
+        self.assertRaises(TypeError,get_column_stats.calculate_stdev,1.0000)
+    def test_empty_list(self):
+        self.assertRaises(IndexError,get_column_stats.calculate_stdev,[])
+    def test_bad_list_value(self):
+        self.assertRaises(TypeError,get_column_stats.calculate_stdev,[1,2,3,'text'])
+    def test_fixed_value(self):
+        self.assertEqual(get_column_stats.calculate_stdev([1,1,1,1,1]),0)
+    def test_random_values(self):
+        for i in range(100):
+            length = random.randint(1,1000)
+            rand = []
+            for j in range(length):
+                rand.append(random.randint(1,1000))
+            self.assertAlmostEqual(get_column_stats.calculate_stdev(rand),np.std(rand))
         
     
