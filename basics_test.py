1334f54ef39e18b370edbcb151b3a1d4d18e1f12
owenmadin <owen.madin@colorado.edu>
2019-10-24 11:31:18 -0600

    unit tests for mean

--- basics_test.py
+++ basics_test.py
@@ -1,5 +1,7 @@
 import unittest
 import get_column_stats
+import random
+import numpy as np
 
 class TestMean(unittest.TestCase):
     def test_no_input(self):
@@ -12,5 +14,15 @@ class TestMean(unittest.TestCase):
         self.assertRaises(IndexError,get_column_stats.calculate_mean,[])
     def test_bad_list_value(self):
         self.assertRaises(TypeError,get_column_stats.calculate_mean,[1,2,3,'text'])
-    
+    def test_fixed_value(self):
+        self.assertEqual(get_column_stats.calculate_mean([1,1,1,1,1]),1)
+    def test_random_values(self):
+        for i in range(100):
+            length = random.randint(1,1000)
+            rand = []
+            for j in range(length):
+                rand.append(random.randint(1,1000))
+            self.assertEqual(get_column_stats.calculate_mean(rand),np.mean(rand))
+            
+        
     
