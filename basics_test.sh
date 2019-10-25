1a6b6415cc1ef874f6748f256fde042376b5a0ff
owenmadin <owen.madin@colorado.edu>
2019-10-24 21:06:55 -0600

    Updated functional tests

--- basics_test.sh
+++ basics_test.sh
@@ -16,15 +16,18 @@ done )> data.txt
 
 
 
-run check_argparse_inputs_failure python get_column_stats.py data.txt 2
+run check_argparse_inputs_failure python get_column_stats.py data.txt $a
 assert_stderr
 assert_exit_code 2
 
-
-run check_argparse_inputs_success python get_column_stats.py -f data.txt -c 2
+a=$(( RANDOM % 5))
+run check_argparse_inputs_success python get_column_stats.py -f data.txt -c $a
 assert_no_stderr
 assert_in_stdout stdev
 assert_exit_code 0
+
+run check_bad_data_file python get_column_stats.py -f fake.txt -c $a
+assert_exit_code 1
 
 
 V=1
@@ -32,7 +35,11 @@ V=1
     echo -e "$V\t$V\t$V\t$V\t$V";
 done )> data.txt
 
-run check_basic_output_stdev python get_column_stats.py -f data.txt -c 2
-assert_in_stdout mean: 1
-assert_in_stdout stdev: 0
+run check_basic_output_stdev python get_column_stats.py -f data.txt -c $a
+assert_in_stdout 'mean: 1'
+assert_in_stdout 'stdev: 0'
 assert_exit_code 0
+
+#teardown
+rm ssshtest
+rm data.txt
